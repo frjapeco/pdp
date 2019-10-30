17e0a54d0097702bc152d9cfdf738bc2ba015dd6
Javier Perez <franciscojavier.perez.costas.next@bbva.com>
2019-10-30 11:02:02 +0100

    DecisionController refactorized

--- src/main/java/com/bbva/ether/secaas/authz/pdp/rest/DecisionController.java
+++ src/main/java/com/bbva/ether/secaas/authz/pdp/rest/DecisionController.java
@@ -1,10 +1,8 @@
 package com.bbva.ether.secaas.authz.pdp.rest;
 
-import com.bbva.ether.secaas.authz.pdp.jsonprofile.model.Result;
 import com.bbva.ether.secaas.authz.pdp.rest.dto.DecisionRequest;
 import com.bbva.ether.secaas.authz.pdp.rest.dto.DecisionResponse;
 import com.bbva.ether.secaas.authz.pdp.services.DecisionService;
-import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,10 +19,8 @@ public class DecisionController {
 
     @PostMapping
     public Mono<DecisionResponse> evaluate(@RequestBody DecisionRequest request) {
-        Mono<Result[]> response = decisionService.evaluate(request.getRequest());
-        return response.map(x -> DecisionResponse.builder()
-                .response(x)
-                .build());
+        return decisionService.evaluate(request.getRequest())
+                .map(x -> DecisionResponse.builder().response(x).build());
     }
 
 }
