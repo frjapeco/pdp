8f56b2f10bc3053b92c0b1c587de947bb6e646c1
Javier Perez <franciscojavier.perez.costas.next@bbva.com>
2019-12-10 13:04:17 +0100

    Custom attribute finder removed from configuration

--- src/main/java/com/bbva/ether/secaas/authz/pdp/config/ApplicationConfig.java
+++ src/main/java/com/bbva/ether/secaas/authz/pdp/config/ApplicationConfig.java
@@ -1,16 +1,11 @@
 package com.bbva.ether.secaas.authz.pdp.config;
 
-import com.bbva.ether.secaas.authz.pdp.balana.CustomAttributeFinder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.wso2.balana.Balana;
 import org.wso2.balana.PDP;
 import org.wso2.balana.PDPConfig;
-import org.wso2.balana.finder.AttributeFinder;
-import org.wso2.balana.finder.AttributeFinderModule;
-
-import java.util.List;
 
 import static org.wso2.balana.finder.impl.FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY;
 
@@ -25,11 +20,7 @@ public class ApplicationConfig {
         System.setProperty(POLICY_DIR_PROPERTY, policiesLocation);
         Balana balana = Balana.getInstance();
         PDPConfig pdpConfig = balana.getPdpConfig();
-        AttributeFinder attributeFinder = pdpConfig.getAttributeFinder();
-        List<AttributeFinderModule> finderModules = attributeFinder.getModules();
-        finderModules.add(new CustomAttributeFinder());
-        attributeFinder.setModules(finderModules);
-        return new PDP(new PDPConfig(attributeFinder, pdpConfig.getPolicyFinder(), null, true));
+        return new PDP(pdpConfig);
     }
 
 }
